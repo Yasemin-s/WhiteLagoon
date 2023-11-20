651410c49915a9ef6e10ff54359781d112414b12
Yasemin-s <l2012721036@isparta.edu.tr>
2023-11-20 15:12:36 +0300

    vt de rol oluşturma, register ve login işlemleri

--- WhiteLagoon.Web/Controllers/AccountController.cs
+++ WhiteLagoon.Web/Controllers/AccountController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using WhiteLagoon.Application.Common.Interfaces;
+using WhiteLagoon.Application.Common.Utility;
 using WhiteLagoon.Domain.Entities;
 using WhiteLagoon.Web.ViewModels;
 
@@ -41,14 +43,121 @@ namespace WhiteLagoon.Web.Controllers
         public IActionResult Register()
         {
             //kural yoksa olustuacak - rol kotnrolu yapiyor, yoksa ikisini de yarat dedik.
-            if (!_roleManager.RoleExistsAsync("Admin").GetAwaiter().GetResult())
+            //SD.role_Admin diyerek, sd sinifindaki admin i kontrol etmek istedigimizi soyledik.
+            if (!_roleManager.RoleExistsAsync(SD.Role_Admin).GetAwaiter().GetResult())
             {
                 //admn adinda asenkron islemin bitmesi beklenerek rol olusturulmustur.
-                _roleManager.CreateAsync(new IdentityRole("Admin")).Wait();
-                _roleManager.CreateAsync(new IdentityRole("Customer")).Wait();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Admin)).Wait();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Customer)).Wait();
             }
 
-            return View();
+            RegisterVM registerVM = new()
+            {
+                RoleList = _roleManager.Roles.Select(x => new SelectListItem
+                {
+                    Text = x.Name,
+                    Value = x.Name
+                })
+            };
+
+            return View(registerVM);
         }
+ 
+
+        //kullanıcıyı vt ye kaydetmek icin kullanici verilerini alip esitledik.
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterVM registerVM)
+        {
+            
+            ApplicationUser user = new()
+            {
+                Name = registerVM.Name,
+                Email = registerVM.Email,
+                PhoneNumber = registerVM.PhoneNumber,
+                NormalizedEmail = registerVM.Email.ToUpper(),
+                EmailConfirmed = true,
+                UserName = registerVM.Email,
+                CreatedAt = DateTime.Now    //hesabin ne zaman olsuturuldugu bilgisini tasir.
+            };
+
+            //ef yyerine, yardimci metot kullandil.
+            var result = await _userManager.CreateAsync(user, registerVM.Password);
+            
+            //kullanıcı olusturma basarili ise
+            if(result.Succeeded)
+            {
+                //rol secimi yapilmissa, secilen rol atanacaktir.
+                if(!string.IsNullOrEmpty(registerVM.Role))
+                {
+                    //kullanicinin secmis oldugu rolu atar
+                    await _userManager.AddToRoleAsync(user, registerVM.Role);
+                }
+                else
+                {
+                    //kullanici rol secmemisse default customer atar
+                    await _userManager.AddToRoleAsync(user, SD.Role_Customer);
+                }
+           
+
+                //oturum acma kismi , presistent ise tarayiciyi kapattigi anda oturumu sonlandirir, yani giris icin tekrar bilgi ister.
+                await _signInManager.SignInAsync(user, isPersistent:false);
+
+                if(string.IsNullOrEmpty(registerVM.RedirectUrl)) 
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    return LocalRedirect(registerVM.RedirectUrl);
+                }
+
+                foreach(var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+            //registerVM sinifinin RoleList ozelligine rol ekler
+            registerVM.RoleList = _roleManager.Roles.Select(x => new SelectListItem
+                {
+                    Text = x.Name,
+                    Value = x.Name
+                });
+
+            return View(registerVM);
+        }
+
+        //Login
+        public async Task<IActionResult> Login(LoginVM loginVM)
+        {
+            if (ModelState.IsValid)
+            {
+                //basarisiz giris sonunda hesap kitlenmez.
+                var result = await _signInManager
+                    .PasswordSignInAsync(loginVM.Email, loginVM.Password, loginVM.RememberMe, lockoutOnFailure: false);
+
+
+
+                //kullanıcı olusturma basarili ise
+                if (result.Succeeded)
+                {
+                    if (string.IsNullOrEmpty(loginVM.RedirectUrl))
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        return LocalRedirect(loginVM.RedirectUrl);
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Invalid login attempt.");
+                }
+            }
+
+            return View(loginVM);
+        }
+
     }
+
 }
--- WhiteLagoon.Web/ViewModels/RegisterVM.cs
+++ WhiteLagoon.Web/ViewModels/RegisterVM.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
 
 namespace WhiteLagoon.Web.ViewModels
 {
@@ -11,7 +13,9 @@ namespace WhiteLagoon.Web.ViewModels
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
+        [Required]
         [Compare(nameof(Password))] //sifrelerin ayni olup olmadigini kontrol edecek
+        [DataType(DataType.Password)]
         [Display(Name = "Confirm Password")]
         public string ConfirmPassword { get; set; }
 
@@ -22,8 +26,12 @@ namespace WhiteLagoon.Web.ViewModels
         [Display(Name="Phone Number")]
         public string? PhoneNumber { get; set; }
 
-
         public string? RedirectUrl { get; set; }    //sadece oturum açmış kullanıcıların sahip olduğu sayfaya yönlendirme yapmak için
+
+        public string? Role { get; set; }   //RoleList icin baglayici olarak kullanildi - secili degeri tutmak icin tanimalndi.
+
+        [ValidateNever]
+        public IEnumerable<SelectListItem> RoleList { get; set; }   //ilgili kismi role ozelligine baglayip rolelist i cairip dropdown islemi yaptik.
 
     }
 }
